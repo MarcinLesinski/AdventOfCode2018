da459f084a371bb7041b55de46bd1db5a9a2e87d
Marcin Lesiński <marcin.lesinski.87@gmail.com>
2019-01-31 01:25:10 +0100

    day1 part1 1st test added.

--- tests/day1/Part1Test.java
+++ tests/day1/Part1Test.java
@@ -1,6 +1,32 @@
-import static org.junit.Assert.*;
+package day1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Part1Test
 {
 
+    Part1 riddle;
+
+
+    @Test
+    public void solve()
+    {
+        List<String> data = new ArrayList<>();
+        data.add("+1");
+        data.add("+1");
+        data.add("+1");
+        riddle = new Part1(data);
+        riddle.setOnLog(text -> {});
+        AtomicReference<String> actual = new AtomicReference<>();
+        riddle.setOnResult(result -> actual.set(result));
+        riddle.solve();
+
+        String expected = "3";
+        Assert.assertEquals(expected, actual.toString());
+    }
 }
