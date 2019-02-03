99e8fd040c2cb37362db4c1cc90ede8280950ee2
Marcin Lesiński <marcin.lesinski.87@gmail.com>
2019-02-03 19:49:23 +0100

    Day1 Part2 implemented and tested.

--- src/day1/Part2.java
+++ src/day1/Part2.java
@@ -1,10 +1,11 @@
 package day1;
 
-import Riddles.OnLog;
 import Riddles.Riddle;
 import Riddles.RiddleBase;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Part2 extends RiddleBase implements Riddle
 {
@@ -13,15 +14,27 @@ public class Part2 extends RiddleBase implements Riddle
         super(data);
     }
 
+    //TODO: It may take long time, use thread
     @Override
     public void solve()
     {
-
-    }
-
-    @Override
-    public void setOnLog(OnLog onlog)
-    {
-
+        Set<Integer> occured = new HashSet<>();
+        Integer frequency = 0;
+        occured.add(frequency);
+        while (true)
+        {
+            for (int i = 0; i < data.size(); i++)
+            {
+                frequency += Integer.parseInt(data.get(i));
+                if (occured.contains(frequency))
+                {
+                    onResult.onResult(frequency.toString());
+                    return;
+                } else
+                {
+                    occured.add(frequency);
+                }
+            }
+        }
     }
 }
--- tests/day1/Part2Test.java
+++ tests/day1/Part2Test.java
@@ -1,6 +1,52 @@
-import static org.junit.Assert.*;
+package day1;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+@RunWith(Parameterized.class)
 public class Part2Test
 {
+    private Part2 riddle;
+    private List<String> input;
+    private String expected;
 
+    public Part2Test(List<String> input, String expetced)
+    {
+        this.input = input;
+        this.expected = expetced;
+    }
+
+    @Parameterized.Parameters
+    public static Collection cases()
+    {
+        return Arrays.asList(new Object[][]{
+                {new ArrayList<>(Arrays.asList("+1", "-1")), "0"},
+                {new ArrayList<>(Arrays.asList("+3", "+3", "+4", "-2", "-4")), "10"},
+                {new ArrayList<>(Arrays.asList("-6", "+3", "+8", "+5", "-6")), "5"},
+                {new ArrayList<>(Arrays.asList("+7", "+7", "-2", "-7", "-4")), "14"}
+        });
+    }
+
+
+    @Test
+    public void solve()
+    {
+        riddle = new Part2(input);
+        riddle.setOnLog(text ->
+        {
+        });
+        AtomicReference<String> actual = new AtomicReference<>();
+        riddle.setOnResult(actual::set);
+        riddle.solve();
+
+        Assert.assertEquals(this.expected, actual.toString());
+    }
 }
