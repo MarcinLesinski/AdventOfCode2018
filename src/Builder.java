b2dc57a61913b8a6c8205c2586958901f5057eeb
Marcin Lesiński <marcin.lesinski.87@gmail.com>
2019-02-03 19:47:25 +0100

    Fixed path in riddles Builder.

--- src/Builder.java
+++ src/Builder.java
@@ -9,8 +9,8 @@ public class Builder
     {
 
         riddles.put(1, 1, day1.Part1.class, "./res/data_day1.txt");
-        riddles.put(1, 2, day1.Part2.class, "data_day1.txt");
-        riddles.put(2, 1, day2.Part1.class, "data_day2.txt");
+        riddles.put(1, 2, day1.Part2.class, "./res/data_day1.txt");
+        riddles.put(2, 1, day2.Part1.class, "./res/data_day2.txt");
         return riddles;
     }
 }
