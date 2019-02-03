b401c71fb85a3dd5ca66d7a782beb59777b3e7fa
Marcin Lesiński <marcin.lesinski.87@gmail.com>
2019-02-03 18:50:24 +0100

    Part1Test tests replaced with parameterized test.

--- tests/day1/Part1Test.java
+++ tests/day1/Part1Test.java
@@ -2,31 +2,50 @@ package day1;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+@RunWith(Parameterized.class)
 public class Part1Test
 {
 
-    Part1 riddle;
+    private Part1 riddle;
+    private List<String> input;
+    private String expected;
 
+    public Part1Test(List<String> input, String expetced)
+    {
+        this.input = input;
+        this.expected = expetced;
+    }
+
+    @Parameterized.Parameters
+    public static Collection cases()
+    {
+        return Arrays.asList(new Object[][]{
+                {new ArrayList<>(Arrays.asList("+1", "+1", "+1")), "3"},
+                {new ArrayList<>(Arrays.asList("+1", "+1", "-2")), "0"},
+                {new ArrayList<>(Arrays.asList("-1", "-2", "-3")), "-6"}
+        });
+    }
 
     @Test
     public void solve()
     {
-        List<String> data = new ArrayList<>();
-        data.add("+1");
-        data.add("+1");
-        data.add("+1");
-        riddle = new Part1(data);
-        riddle.setOnLog(text -> {});
+        riddle = new Part1(input);
+        riddle.setOnLog(text ->
+        {
+        });
         AtomicReference<String> actual = new AtomicReference<>();
-        riddle.setOnResult(result -> actual.set(result));
+        riddle.setOnResult(actual::set);
         riddle.solve();
 
-        String expected = "3";
-        Assert.assertEquals(expected, actual.toString());
+        Assert.assertEquals(this.expected, actual.toString());
     }
 }
