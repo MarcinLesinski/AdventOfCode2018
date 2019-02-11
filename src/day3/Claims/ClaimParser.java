be3099ee6d0f797e046c7b6d3672f6541af24a5a
Marcin Lesiński <marcin.lesinski.87@gmail.com>
2019-02-11 21:13:09 +0100

    Day3 ClaimsParser bugfix.

--- src/day3/Claims/ClaimParser.java
+++ src/day3/Claims/ClaimParser.java
@@ -5,12 +5,12 @@ class ClaimParser
     public static Claim parse(String rawDataLine)
     {
         Claim claim = new Claim();
-        String[] parts = rawDataLine.split("@|,|:|x");
-        claim.id = Integer.parseInt(parts[0].trim());
-        claim.left = Integer.parseInt(parts[1].trim());
-        claim.top = Integer.parseInt(parts[2].trim());
-        claim.width = Integer.parseInt(parts[3].trim());
-        claim.height = Integer.parseInt(parts[4].trim());
+        String[] parts = rawDataLine.split("#|@|,|:|x");
+        claim.id = Integer.parseInt(parts[1].trim());
+        claim.left = Integer.parseInt(parts[2].trim());
+        claim.top = Integer.parseInt(parts[3].trim());
+        claim.width = Integer.parseInt(parts[4].trim());
+        claim.height = Integer.parseInt(parts[5].trim());
         return claim;
     }
 }
