80add7817a367669fe7421b9c55a297524f47f80
Marcin Lesiński <marcin.lesinski.87@gmail.com>
2019-02-12 23:41:06 +0100

    Day4/part1 scheme added.

---
+++ src/day4/Part1.java
@@ -0,0 +1,20 @@
+package day4;
+
+import Riddles.Riddle;
+import Riddles.RiddleBase;
+
+import java.util.List;
+
+public class Part1  extends RiddleBase implements Riddle
+{
+    public Part1(List<String> rawData)
+    {
+        super(rawData);
+    }
+
+    @Override
+    public void solve()
+    {
+
+    }
+}
